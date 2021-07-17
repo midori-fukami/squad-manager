d697d5dc5c870fb14a701fa27b84fad9f54c8f35
Midori Fukami <midori.fukami@hotmail.com>
2021-07-16 22:23:09 -0300

    Add Login validator

---
+++ SquadManager/Validator/UserValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+using SquadManager.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SquadManager.Validator
+{
+    public class UserValidator : AbstractValidator<UserViewModel>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.Email).NotNull().WithMessage("E-mail vazio");
+            RuleFor(user => user.Email).EmailAddress().WithMessage("O e-mail está inválido");
+            RuleFor(user => user.Password).NotNull().WithMessage("Digite a senha");
+        }
+    }
+}
