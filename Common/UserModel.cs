4196079d15c1db0c9bc208c1d5f3d1b575914204
Midori Fukami <midori.fukami@hotmail.com>
2021-08-04 00:10:48 -0300

    remove

--- Common/UserModel.cs
+++ Common/UserModel.cs
@@ -2,12 +2,8 @@
 {
     public class UserModel
     {
-        public string username { get; set; }
-
         public string Email { get; set; }
 
         public string Password { get; set; }
-
-        public string ConfirmPassword { get; set; }
     }
 }
