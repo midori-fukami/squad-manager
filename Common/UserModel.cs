2084ae522514a121a1ddd9e2c98563bd7beb9281
Midori Fukami <midori.fukami@hotmail.com>
2021-08-12 23:20:07 -0300

    Add reset view

--- Common/UserModel.cs
+++ Common/UserModel.cs
@@ -9,5 +9,7 @@
         public string Password { get; set; }
 
         public string ConfirmPassword { get; set; }
+
+        public string Id { get; set; }
     }
 }
---
+++ SquadManager/wwwroot/js/reset.js
@@ -0,0 +1,39 @@
+﻿$('form').on('submit', function (event) {
+    event.preventDefault();
+
+    if ($("#password").val() != $("#confirmPassword").val()) {
+        $(".error span").show();
+
+        setTimeout(function () {
+            $(".error span").hide();
+        }, 3000)
+
+        return;
+    }
+
+    const urlParam = new URLSearchParams(window.location.search);
+    const id = urlParam.get('id');
+
+    var formData = {
+        password: $("#password").val(),
+        confirmPassword: $("#confirmPassword").val(),
+        id: id
+    }
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify(formData),
+        url: "https://localhost:44317/api/User/reset",
+        success: function (result) {
+            if (result.response == 'OK')
+                alert("Senha redefinida");
+            else
+                alert("Erro inesperado");
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+});
