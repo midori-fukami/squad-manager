00aec7bed2e4d88682d2c25f01b6c0c07e49b72f
Midori Fukami <midori.fukami@hotmail.com>
2021-08-19 22:26:24 -0300

    Add context and UserEntity

---
+++ API/Context/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Repository.Entity;
+
+namespace API.Context
+{
+    public class EFContext : DbContext
+    {
+        private const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=EFCore;Trusted_Connection=True;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
---
+++ Repository/Entity/UserEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Entity
+{
+    public class UserEntity
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Id { get; set; }
+    }
+}
