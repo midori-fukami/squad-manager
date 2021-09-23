c1eea950d60728babd4029edc08caac233564986
Midori Fukami <midori.fukami@hotmail.com>
2021-09-22 22:48:53 -0300

    Create menu, add User edit view and controller

--- SquadManager/wwwroot/js/site.js
+++ SquadManager/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function changeMenu(menuToChange) {
+    $('nav').find('li.btn-active').removeClass('btn-active');
 
-// Write your JavaScript code.
+    if (menuToChange != 'home') {
+        $('nav [data-menu="' + menuToChange + '"]').addClass('btn-active');
+    }
+}
